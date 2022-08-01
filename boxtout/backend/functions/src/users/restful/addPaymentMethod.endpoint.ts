a4bcf590da66389ce9aaf1c8373c03418239e6b0
symphony <jciurc@gmail.com>
2022-07-31 18:09:06 -0700

    set up functions index, and user payment endpoint

---
+++ boxtout/backend/functions/src/users/restful/addPaymentMethod.endpoint.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { Endpoint, RequestType } from 'firebase-backend';
+
+export default new Endpoint('addPaymentMethod', RequestType.POST, (request: Request, response: Response) => {
+  const cardNumber = request.body['card_number'];
+  const cardHolder = request.body['card_holder'];
+
+  const paymentToken = `${cardNumber}_${cardHolder}`;
+
+  return response.status(201).send({ token: paymentToken });
+});
